3dcbc86f27062208668e422e9d4a1f3fa75e8c27
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-22 17:10:31 +0100

    fetch method + event listener on submit

--- index.js
+++ index.js
@@ -25,3 +25,23 @@ const dateInput=document.getElementById("date-input")
 const submitBtn=document.getElementById("submit-btn")
 const eventsDiv=document.getElementById("events")
 
+submitBtn.addEventListener("click", () => {
+    const date=dateInput.ariaValue
+    const url=`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date}`
+
+    fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${process.env.ACCESSTOKEN}`,
+            'Api-User-Agent': `${process.env.APPNAME} (${process.env.CONTACT})`
+        }
+    })
+        .then(response=>response.json)
+        .then((data) => {
+            eventsDiv.innerHTML=""
+            data.events.forEach(event=> {
+                const eventEl=document.createElement("div")
+                eventEl.textContent=`${event.text} (Earliest date: ${event.earliest})`
+                eventsDiv.appendChild(eventEl)
+            })
+        })
+})
