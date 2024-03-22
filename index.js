0abf95fc6832a971e2751344eed5b15fd4231868
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-22 17:17:28 +0100

    Formatting

--- index.js
+++ index.js
@@ -1,51 +1,48 @@
 // Get information about this day in history from English Wikipedia
 
 let today = new Date();
-let month = String(today.getMonth() + 1).padStart(2,'0');
-let day = String(today.getDate()).padStart(2,'0');
+let month = String(today.getMonth() + 1).padStart(2, "0");
+let day = String(today.getDate()).padStart(2, "0");
 let url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
 
-let response = await fetch( url,
-    {
-        headers: {
-            'Authorization': `Bearer ${process.env.ACCESSTOKEN}`,
-            'Api-User-Agent': `${process.env.APPNAME} (${process.env.CONTACT})`
-        }
-    }
-);
-response.json()
-    .then(console.log).catch(console.error);
+let response = await fetch(url, {
+  headers: {
+    Authorization: `Bearer ${process.env.ACCESSTOKEN}`,
+    "Api-User-Agent": `${process.env.APPNAME} (${process.env.CONTACT})`,
+  },
+});
+response.json().then(console.log).catch(console.error);
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 
-const dateInput=document.getElementById("date-input")
-const submitBtn=document.getElementById("submit-btn")
-const eventsDiv=document.getElementById("events")
+const dateInput = document.getElementById("date-input");
+const submitBtn = document.getElementById("submit-btn");
+const eventsDiv = document.getElementById("events");
 
 submitBtn.addEventListener("click", () => {
-    const date=dateInput.ariaValue
-    const url=`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date}`
+  const date = dateInput.ariaValue;
+  const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date}`;
 
-    fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${process.env.ACCESSTOKEN}`,
-            'Api-User-Agent': `${process.env.APPNAME} (${process.env.CONTACT})`
-        }
+  fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.ACCESSTOKEN}`,
+      "Api-User-Agent": `${process.env.APPNAME} (${process.env.CONTACT})`,
+    },
+  })
+    .then((response) => response.json)
+    .then((data) => {
+      eventsDiv.innerHTML = "";
+      data.events.forEach((event) => {
+        const eventEl = document.createElement("div");
+        eventEl.textContent = `${event.text} (Earliest date: ${event.earliest})`;
+        eventsDiv.appendChild(eventEl);
+      });
     })
-        .then(response=>response.json)
-        .then((data) => {
-            eventsDiv.innerHTML=""
-            data.events.forEach(event=> {
-                const eventEl=document.createElement("div")
-                eventEl.textContent=`${event.text} (Earliest date: ${event.earliest})`
-                eventsDiv.appendChild(eventEl)
-            })
-        })
-        .catch(err=> {
-            console.log(err)
-            events.Div.textContent=`Error: Unable to retrieve events. (${err})`
-        })
-})
+    .catch((err) => {
+      console.log(err);
+      events.Div.textContent = `Error: Unable to retrieve events. (${err})`;
+    });
+});
