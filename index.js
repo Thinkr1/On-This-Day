98e1fc35e518b45feface7320d64e2262ad36cee
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-22 17:14:37 +0100

    Transform on change

--- index.js
+++ index.js
@@ -21,31 +21,30 @@ response.json()
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 
-const dateInput=document.getElementById("date-input")
-const submitBtn=document.getElementById("submit-btn")
-const eventsDiv=document.getElementById("events")
+const dateInput = document.getElementById('date-input');
+const eventsDiv = document.getElementById('events');
 
-submitBtn.addEventListener("click", () => {
-    const date=dateInput.ariaValue
-    const url=`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date}`
+function getEvents() {
+  const date = dateInput.value;
+  const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${date}`;
 
-    fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${process.env.ACCESSTOKEN}`,
-            'Api-User-Agent': `${process.env.APPNAME} (${process.env.CONTACT})`
-        }
-    })
-        .then(response=>response.json)
-        .then((data) => {
-            eventsDiv.innerHTML=""
-            data.events.forEach(event=> {
-                const eventEl=document.createElement("div")
-                eventEl.textContent=`${event.text} (Earliest date: ${event.earliest})`
-                eventsDiv.appendChild(eventEl)
-            })
-        })
-        .catch(err=> {
-            console.log(err)
-            events.Div.textContent=`Error: Unable to retrieve events. (${err})`
-        })
-})
+  fetch(url, {
+    headers: {
+        'Authorization': `Bearer ${process.env.ACCESSTOKEN}`,
+        'Api-User-Agent': `${process.env.APPNAME} (${process.env.CONTACT})`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    eventsDiv.innerHTML = '';
+    data.events.forEach(event => {
+      const eventEl = document.createElement('div');
+      eventEl.textContent = `${event.text} (Earliest date: ${event.earliest})`;
+      eventsDiv.appendChild(eventEl);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    eventsDiv.textContent = 'Error: Unable to retrieve events.';
+  });
+}
